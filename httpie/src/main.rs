60944b8594af7e18d53b580d30e7531dbad112fc
chenwochong <chenwochong@163.com>
2025-05-31 22:48:55 +0800

    finish clap

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,24 +1,35 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(version = "1.0.0", author = "chenwochong")]
 struct Opts {
-    #[arg(short, long)]
-    name: String,
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[command(subcommand)]
+    subcmd: SubCommand,
 }
 
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    /// get request
+    Get(Get),
+    /// post request
+    Post(Post),
+}
+
+#[derive(Parser, Debug)]
 struct Get {
+    /// http request url
     url: String,
 }
 
+#[derive(Parser, Debug)]
 struct Post {
+    /// http request url
     url: String,
+    /// http request body
     body: Vec<String>,
 }
 
 fn main() {
     let opt = Opts::parse();
-    println!("Hello, world! {}, count {}", opt.name, opt.count);
+    println!("Hello, world! {:?}", opt);
 }
