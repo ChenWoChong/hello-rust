c9dd3f02baffe784e40e1e82cd826a93e8eed497
chenwochong <chenwochong@163.com>
2025-06-02 09:54:23 +0800

    add some func

--- thumbor/src/pb/mod.rs
+++ thumbor/src/pb/mod.rs
@@ -1,11 +1,12 @@
 mod abi;
+pub use abi::*;
 use base64::{
     Engine, alphabet,
     engine::{self, general_purpose},
 };
-
-pub use abi::*;
+use photon_rs::transform::SamplingFilter;
 use prost::Message;
+
 const CUSTOM_ENGINE: engine::GeneralPurpose =
     engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
@@ -30,4 +31,79 @@ impl TryFrom<&str> for ImageSpec {
     }
 }
 
+impl filter::Filter {
+    pub fn to_str(self) -> Option<&'static str> {
+        match self {
+            filter::Filter::Unspecified => None,
+            filter::Filter::Oceanic => Some("oceanic"),
+            filter::Filter::Islands => Some("islands"),
+            filter::Filter::Marine => Some("marine"),
+        }
+    }
+}
 
+impl From<resize::SampleFilter> for SamplingFilter {
+    fn from(value: resize::SampleFilter) -> Self {
+        match value {
+            resize::SampleFilter::Undefined => SamplingFilter::Nearest,
+            resize::SampleFilter::Nearest => SamplingFilter::Nearest,
+            resize::SampleFilter::Triangle => SamplingFilter::Triangle,
+            resize::SampleFilter::CatmullRom => SamplingFilter::CatmullRom,
+            resize::SampleFilter::Gaussian => SamplingFilter::Gaussian,
+            resize::SampleFilter::Lanczos3 => SamplingFilter::Lanczos3,
+        }
+    }
+}
+
+impl Spec {
+    pub fn new_resize_seam_carve(width: u32, height: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width,
+                height,
+                r_type: resize::ResizeType::SeamCarve as i32,
+                filter: resize::SampleFilter::Undefined as i32,
+            })),
+        }
+    }
+
+    pub fn new_resize(width: u32, height: u32, filter: resize::SampleFilter) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width,
+                height,
+                r_type: resize::ResizeType::Normal as i32,
+                filter: filter as i32,
+            })),
+        }
+    }
+
+    pub fn new_filter(filter: filter::Filter) -> Self {
+        Self {
+            data: Some(spec::Data::Filter(Filter {
+                filter: filter as i32,
+            })),
+        }
+    }
+
+    pub fn new_watermark(x: u32, y: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Watermark(Watermark { x, y })),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::borrow::Borrow;
+
+    #[test]
+    fn encoded_spec_could_be_decoded() {
+        let spec1 = Spec::new_resize(600, 600, resize::SampleFilter::CatmullRom);
+        let spec2 = Spec::new_filter(filter::Filter::Marine);
+        let image_spec = ImageSpec::new(vec![spec1, spec2]);
+        let s: String = image_spec.borrow().into();
+        assert_eq!(image_spec, s.as_str().try_into().unwrap())
+    }
+}
