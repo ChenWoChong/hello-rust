189076f72bd0ea3c6d761cd420c601a8325b1555
chenwochong <chenwochong@163.com>
2025-06-14 08:08:34 +0800

    add 09

---
+++ app/src/owner/mod.rs
@@ -0,0 +1,5 @@
+mod borrow;
+mod node;
+
+pub use borrow::*;
+pub use node::test_up_ds;
---
+++ app/src/owner/node.rs
@@ -0,0 +1,41 @@
+use std::rc::Rc;
+
+#[derive(Debug)]
+#[allow(unused)]
+struct Node {
+    id: usize,
+    downstream: Option<Rc<Node>>,
+}
+
+#[allow(unused)]
+impl Node {
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            downstream: None,
+        }
+    }
+
+    pub fn update_downstream(&mut self, ds: Rc<Node>) {
+        self.downstream = Some(ds)
+    }
+
+    pub fn get_ds(&self) -> Option<Rc<Node>> {
+        self.downstream.clone()
+    }
+}
+
+pub fn test_up_ds() {
+    let mut node1 = Node::new(1);
+    let mut node2 = Node::new(2);
+    let mut node3 = Node::new(3);
+    let node4 = Node::new(4);
+    node3.update_downstream(Rc::new(node4));
+
+    node1.update_downstream(Rc::new(node3));
+    node2.update_downstream(node1.get_ds().unwrap());
+    println!("node1:{:#?},\nnode2:{:#?}", node1, node2);
+}
+
+#[cfg(test)]
+mod tests {}
