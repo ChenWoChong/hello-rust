a3904923eda9b87e70b81c36a9e04aebc30cbcea
chenwochong <chenwochong@163.com>
2025-08-02 23:01:18 +0800

    change opt test_hash_map

--- app/src/generic/hash_map.rs
+++ app/src/generic/hash_map.rs
@@ -1,23 +1,42 @@
 use std::collections::HashMap;
 
 pub fn test_hash_map() {
-    let map = HashMap::new();
-    let mut map = explain("empty", map);
-    
+    let mut map = HashMap::new();
+    explain("empty", &map);
+
     map.insert('a', 1);
-    let mut map = explain("added 1", map);
-    
+    explain("added 1", &map);
+
     map.insert('b', 2);
     map.insert('c', 3);
-    let mut map = explain("added 3", map);
+    explain("added 3", &map);
     
+    map.insert('d', 4);
+    explain("added 4", &map);
+    
+    assert_eq!(map.get(&'a'), Some(&1));
+    assert_eq!(map.get_key_value(&'b'), Some((&'b', &2)));
+    
+    map.remove(&'a');
+    assert_eq!(map.contains_key(&'a'), false);
+    assert_eq!(map.get(&'a'), None);
+    explain("removed", &map);
+    
+    map.shrink_to_fit();
+    explain("shrinked", &map);
 }
 
-fn explain<K, V>(name: &str, map: HashMap<K, V>) -> HashMap<K, V> {
-    let arr: [usize; 6] = unsafe { std::mem::transmute(map) };
+fn explain<K, V>(name: &str, map: &HashMap<K, V>) {
+    let capacity = map.capacity();
+    let items = map.len();
+
+    let bucket_mask = if capacity == 0 { 0 } else { capacity - 1 };
+
     println!(
-        "[{}]:\n\tbucket_mask 0x{:x}, ctrl px{:x}, grown_left: {}, items: {}",
-        name, arr[2], arr[3], arr[4], arr[5]
+        "[{name}]:\n\t(公开) capacity: {capacity}, len: {items},\t(推断) bucket_mask: 0x{bucket_mask:x}",
+        name = name,
+        items = items,
+        bucket_mask = bucket_mask,
+        capacity = capacity,
     );
-    unsafe { std::mem::transmute(arr) }
 }
