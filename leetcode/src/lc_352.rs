92c142ab1af7012a003172c43985f53930caeea6
chenwochong <chenwochong@163.com>
2025-08-09 09:58:30 +0800

    opt lc_352

--- leetcode/src/lc_352.rs
+++ leetcode/src/lc_352.rs
@@ -15,34 +15,51 @@ impl SummaryRanges {
     }
 
     fn add_num(&mut self, value: i32) {
-        let mut neighbors: Vec<_> = self
+        let prev = self
             .range
-            .range(..=value + 1)
-            .last()
-            .into_iter()
-            .chain(self.range.range(value..).next())
-            .map(|(&k, &v)| (k, v))
-            .collect();
-        neighbors.dedup();
+            .range(..=value)
+            .next_back()
+            .map(|(&start, &end)| (start, end));
+        let next = self
+            .range
+            .range(value..)
+            .next()
+            .map(|(&start, &end)| (start, end));
 
-        let (mut start, mut end) = (value, value);
-
-        for (ns, ne) in neighbors {
-            if ns <= value && value <= ne {
+        if let Some((_, end)) = prev {
+            if value <= end {
                 return;
-            }
-
-            if ne + 1 == value {
-                start = ns;
-                self.range.remove(&ns);
-            }
-            if ns - 1 == value {
-                end = ne;
-                self.range.remove(&ns);
             }
         }
 
-        self.range.insert(start, end);
+        let can_merge_prev = prev.map_or(false, |(_, end)| end + 1 == value);
+        let can_merge_next = next.map_or(false, |(start, _)| start - 1 == value);
+
+        match (can_merge_prev, can_merge_next) {
+            (true, true) => {
+                let (pre_start, _) = prev.unwrap();
+                let (next_start, next_end) = next.unwrap();
+
+                // 无需 remove ，直接即可更新
+                // self.range.remove(&pre_start);
+                self.range.remove(&next_start);
+                self.range.insert(pre_start, next_end);
+            }
+            (true, false) => {
+                let (pre_start, _) = prev.unwrap();
+                // 无需 remove ，直接即可更新
+                // self.range.remove(&pre_start);
+                self.range.insert(pre_start, value);
+            }
+            (false, true) => {
+                let (next_start, next_end) = next.unwrap();
+                self.range.remove(&next_start);
+                self.range.insert(value, next_end);
+            }
+            (false, false) => {
+                self.range.insert(value, value);
+            }
+        }
     }
 
     fn get_intervals(&self) -> Vec<Vec<i32>> {
