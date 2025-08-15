1672aa1045f772240aa8dc5e719122877e1acea9
chenwochong <chenwochong@163.com>
2025-08-15 08:03:36 +0800

    add mod

---
+++ kv1/src/proto/mod.rs
@@ -0,0 +1,41 @@
+use crate::proto::abi::command_request::RequestData;
+use crate::proto::abi::{CommandRequest, Hset, Kvpair, Value, value};
+
+mod abi;
+
+impl CommandRequest {
+    #[allow(dead_code)]
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            })),
+        }
+    }
+}
+
+impl Kvpair {
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Self {
+            value: Some(value::Value::String(s)),
+        }
+    }
+}
+
+impl From<&str> for Value {
+    fn from(v: &str) -> Self {
+        Self {
+            value: Some(value::Value::String(v.into())),
+        }
+    }
+}
