ce489a2af2853d5eb0298b0b1a6a7dd0645b8783
chengong <chenwochong@163.com>
2025-08-21 11:45:58 +0800

    rgrep opt read from stdin

--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -5,6 +5,7 @@ use glob::glob;
 use rayon::prelude::*;
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 
@@ -26,58 +27,87 @@ fn main() -> Result<()> {
     // parse cmd
     let args = CliArgs::parse();
     println!("pattern: {}, path: {:?}", args.pattern, args.paths);
-    let mut paths: Vec<PathBuf> = Vec::new();
-    for path_pattern in &args.paths {
-        match glob(path_pattern) {
-            Ok(path_iter) => {
-                // 将 glob 返回的迭代器中的有效路径，添加到总列表中
-                for entry in path_iter {
-                    if let Ok(path) = entry {
-                        paths.push(path);
-                    } else if let Err(e) = entry {
-                        eprintln!("错误: 处理通配符条目时出错: {}", e);
+
+    if args.paths.len() == 1 && args.paths[0] == "-" {
+        let reader = BufReader::new(io::stdin());
+        process_stream(reader, &args.pattern)?;
+    } else {
+        let mut paths: Vec<PathBuf> = Vec::new();
+        for path_pattern in &args.paths {
+            match glob(path_pattern) {
+                Ok(path_iter) => {
+                    // 将 glob 返回的迭代器中的有效路径，添加到总列表中
+                    for entry in path_iter {
+                        if let Ok(path) = entry {
+                            paths.push(path);
+                        } else if let Err(e) = entry {
+                            eprintln!("错误: 处理通配符条目时出错: {}", e);
+                        }
                     }
                 }
+                Err(e) => {
+                    eprintln!("错误: 无效的通配符模式 '{}': {}", path_pattern, e);
+                }
             }
-            Err(e) => {
-                eprintln!("错误: 无效的通配符模式 '{}': {}", path_pattern, e);
+        }
+        let all_res = paths
+            .par_iter()
+            .map(|path| process_file(path, &args.pattern))
+            .collect::<Vec<_>>();
+
+        for res_set in all_res {
+            match res_set {
+                Ok(res) => {
+                    for line in res {
+                        print!("{}", line);
+                    }
+                }
+                Err(e) => return Err(e),
             }
         }
     }
-
-    paths.par_iter().for_each(|path| {
-        if let Err(e) = process_file(path, &args.pattern) {
-            eprintln!("Error processing file {}: {}", path.display(), e);
-        }
-    });
-
     Ok(())
 }
 
-fn process_file(path: &Path, pattern: &str) -> Result<()> {
+fn process_stream(reader: impl BufRead, pattern: &str) -> Result<()> {
+    let re = Regex::new(pattern)?;
+    for (line_num, line) in reader.lines().enumerate() {
+        let line = line?;
+        if re.is_match(&line) {
+            let res = highlight_matches(&line, &re)?;
+            print!("{}:{}:\t{}", "StdIN", line_num + 1, res);
+        }
+    }
+    Ok(())
+}
+
+fn process_file(path: &Path, pattern: &str) -> Result<Vec<String>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     let re = Regex::new(pattern)?;
+    let mut res = Vec::new();
+    res.push(format!("[{}]:\n\n", path.display()));
 
     for (line_num, line) in reader.lines().enumerate() {
         let line = line?;
         if let Some(_) = re.find(&line) {
-            highlight_matches(path, line_num, line.as_str(), &re)?;
+            let highlighted = highlight_matches(line.as_str(), &re)?;
+            res.push(format!("{}:{}:\t{}\n", path.display(), line_num, highlighted));
         }
     }
-    Ok(())
+    Ok(res)
 }
 
-fn highlight_matches(path: &Path, line_num: usize, line: &str, re: &Regex) -> Result<()> {
-    print!("{}:{}:\t", path.display(), line_num);
+fn highlight_matches(line: &str, re: &Regex) -> Result<String> {
     let mut last_end = 0;
+    let mut highlight_line = String::new();
 
     for mat in re.find_iter(line) {
-        print!("{}", &line[last_end..mat.start()]);
-        print!("{}", mat.as_str().red().bold());
+        highlight_line.push_str(&line[last_end..mat.start()]);
+        highlight_line.push_str(&mat.as_str().red().bold());
         last_end = mat.end();
     }
-    println!("{}", &line[last_end..]);
+    highlight_line.push_str(&line[last_end..]);
 
-    Ok(())
+    Ok(highlight_line)
 }
