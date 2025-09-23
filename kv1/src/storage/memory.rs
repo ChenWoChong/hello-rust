15a15db992d0b91653cf3261582164b868ecdfd9
chenwochong <chenwochong@163.com>
2025-09-23 12:00:33 +0800

    opt kv1 storgae trait get_iter fn

--- kv1/src/storage/memory.rs
+++ kv1/src/storage/memory.rs
@@ -93,14 +93,39 @@ impl Storage for MemTable {
 
     fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError> {
         // 查询出 dashMap
-        let table = self.get_or_create_table(table);
+        let table = self.get_or_create_table(table).clone();
 
         // 返回迭代器
-        let res = table
-            .iter()
-            .map(|v| Kvpair::new(v.key(), v.value().clone()))
-            .collect::<Vec<_>>();
+        // let res = table.into_iter().map(|v| v.into());
+        let res = StorageIter::new(table.into_iter());
 
-        Ok(Box::new(res.into_iter()))
+        Ok(Box::new(res))
+    }
+}
+
+impl From<(String, Value)> for Kvpair {
+    fn from(value: (String, Value)) -> Self {
+        Kvpair::new(value.0, value.1)
+    }
+}
+
+pub struct StorageIter<T> {
+    data: T,
+}
+
+impl<T> StorageIter<T> {
+    fn new(data: T) -> Self {
+        Self { data }
+    }
+}
+
+impl<T> Iterator for StorageIter<T>
+where
+    T: Iterator,
+    T::Item: Into<Kvpair>,
+{
+    type Item = Kvpair;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.data.next().map(|v| v.into())
     }
 }
