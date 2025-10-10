51e8308e378ea495a6c5338f150b8ac106117de6
chenwochong <chenwochong@163.com>
2025-10-10 11:35:16 +0800

    finish 30

--- ffi/src/unsafe_2.rs
+++ ffi/src/unsafe_2.rs
@@ -1,6 +1,19 @@
+use lazy_static::lazy_static;
 use std::alloc::{GlobalAlloc, Layout, System};
+use std::collections::HashMap;
 use std::mem;
 use std::str::Utf8Chunk;
+use std::sync::Mutex;
+use std::sync::atomic::AtomicUsize;
+
+lazy_static! {
+    static ref STORE: Mutex<HashMap<&'static str, &'static [u8]>> = Mutex::new(HashMap::new());
+}
+
+#[allow(dead_code)]
+static COUNTER: AtomicUsize = AtomicUsize::new(1);
+#[allow(dead_code)]
+static mut COUNTER_SIMPLE: usize = 1;
 
 #[allow(dead_code)]
 unsafe trait Foo {
@@ -29,9 +42,10 @@ impl Bar for Nonsense {
 
 #[cfg(test)]
 mod tests {
-    use crate::unsafe_2::{Bar, COUNTER, Foo, Nonsense};
+    use crate::unsafe_2::{Bar, COUNTER, COUNTER_SIMPLE, Foo, Nonsense, STORE, split};
     #[allow(unused_imports)]
     use std::mem::transmute;
+    use std::sync::atomic::Ordering;
     use std::thread;
 
     #[test]
@@ -59,7 +73,7 @@ mod tests {
     //     let mut age = 18;
     //     let r1 = &age;
     //     let r2 = &mut age;
-    // 
+    //
     //     println!("r1: {}, r2: {}", *r1, *r2);
     // }
 
@@ -81,29 +95,80 @@ mod tests {
     //         let arr: &mut [u8; 8] = transmute(p);
     //         arr
     //     };
-    // 
+    //
     //     data.copy_from_slice(&[1, 2, 3, 4, 5, 6, 7, 8]);
     //     println!("data: {:?}", data);
-    // 
+    //
     //     unsafe { libc::free(transmute(data)) };
     // }
 
     #[test]
     fn test_global_counter() {
-        let t1 = thread::spawn(move || unsafe { COUNTER += 10 });
+        let t1 = thread::spawn(move || unsafe { COUNTER_SIMPLE += 10 });
         let t2 = thread::spawn(move || unsafe {
-            COUNTER *= 10;
+            COUNTER_SIMPLE *= 10;
         });
 
         t2.join().unwrap();
         t1.join().unwrap();
 
-        // unsafe { println!("COUNTER: {}", COUNTER) }
+        // unsafe { println!("COUNTER_SIMPLE: {}", COUNTER_SIMPLE) }
+    }
+
+    #[test]
+    fn test_mutex_hashmap() {
+        let t1 = thread::spawn(move || {
+            let mut store = STORE.lock().unwrap();
+            store.insert("hello", b"world");
+        });
+
+        let t2 = thread::spawn(move || {
+            let mut store = STORE.lock().unwrap();
+            store.insert("goodbye", b"world");
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+
+        println!("store: {:#?}", STORE.lock().unwrap());
+    }
+
+    #[test]
+    fn test_atomic_counter() {
+        let t1 = thread::spawn(move || {
+            COUNTER.fetch_add(10, Ordering::SeqCst);
+        });
+        let t2 = thread::spawn(move || {
+            COUNTER
+                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| Some(v * 10))
+                .unwrap();
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+
+        println!("COUNTER: {}", COUNTER.load(Ordering::Relaxed));
+    }
+
+    #[test]
+    fn test_unsafe_split() {
+        let mut s = "chen! corey".to_string();
+        let r = s.as_mut();
+        if let Some((s1, s2)) = split(r, '!') {
+            println!("s1: {}, s2: {}", s1, s2);
+        }
     }
 }
 
 #[allow(dead_code)]
-static mut COUNTER: usize = 1;
+fn split(s: &str, sep: char) -> Option<(&str, &str)> {
+    let pos = s.find(sep);
+    pos.map(|pos| {
+        let len = s.len();
+        let sep_len = sep.len_utf8();
+        unsafe { (s.get_unchecked(0..pos), s.get_unchecked(pos + sep_len..len)) }
+    })
+}
 
 #[allow(dead_code)]
 pub struct MyAllocator;
